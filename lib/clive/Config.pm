64322558c6d64aae8ae98af377fac0c3de416ca5
legatvs <legatvs@gmail.com>
2009-08-14 13:11:51 +0300

    bump version.

--- lib/clive/Config.pm
+++ lib/clive/Config.pm
@@ -40,7 +40,7 @@ use clive::HostFactory;
 use clive::Compat;
 use clive::Error qw(CLIVE_OK CLIVE_OPTARG);
 
-use constant VERSION => "2.2.5";
+use constant VERSION => "2.2.6";
 
 sub init {
     my $self = shift;

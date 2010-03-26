621ea8aa7d3d78ebfd934e4ce7761d984fe96db6
Toni Gundogdu <legatvs.git@gmail.com>
2010-03-26 20:50:29 +0200

    bump version.

--- lib/clive/Config.pm
+++ lib/clive/Config.pm
@@ -40,7 +40,7 @@ use clive::HostFactory;
 use clive::Compat;
 use clive::Error qw(CLIVE_OK CLIVE_OPTARG);
 
-use constant VERSION => "2.2.10";
+use constant VERSION => "2.2.11";
 
 sub init {
     my $self = shift;

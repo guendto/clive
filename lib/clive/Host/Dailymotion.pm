2bc0a0ab315f344f748fb95dd5b5ec82e2c95539
Toni Gundogdu <legatvs.git@gmail.com>
2011-03-03 14:50:46 +0200

    Dailymotion.pm: Fix format support
    Returned "default" for all videos nonetheless of -f arg.
    Ported from quvi.

--- lib/clive/Host/Dailymotion.pm
+++ lib/clive/Host/Dailymotion.pm
@@ -26,107 +26,105 @@ use strict;
 
 use clive::Error qw(CLIVE_REGEXP CLIVE_NOSUPPORT);
 
-sub new { return bless ({}, shift); }
+sub new { return bless( {}, shift ); }
 
 sub parsePage {
 
-    my ($self, $content, $props) = @_;
+    my ( $self, $content, $props ) = @_;
 
     $$props->video_host("dailymotion");
 
-    if ($$content =~ /SWFObject\("http:/) {
-
-        clive::Log->instance->err(CLIVE_NOSUPPORT,
-            "Looks like a partner video. Refusing to continue.");
-
-        return 1;
-    }
-
-    # Match video ID, title.
-
     my %re = (
-        id    => qr|video/(.*?)_|,
         title => qr|title="(.*?)"|,
+        id    => qr|video/(.*?)_|
     );
 
-    my ($tmp, $lnk);
+    my $r;
+    return 1 if clive::Util::matchRegExps( \%re, \$r, $content ) != 0;
 
-    return 1  if clive::Util::matchRegExps (\%re, \$tmp, $content) != 0;
+    return 1 if getURL( $self, \$r, $content ) != 0;
 
-    # Some videos have only one link available.
-
-    if ($$content =~ /"video", "(.*?)"/)
-        { $lnk = $1; }
-
-    else {
-        my $r;
-        ($r,$lnk) = parseVideoURL ($self, $content);
-        return $r  unless $r == 0;
-    }
-
-    if (not defined $lnk) {
-        clive::Log->instance->err (CLIVE_REGEXP, "no match: video URL");
-        return 1;
-    }
-
-    # Cleanup video link.
-
-    $lnk =~ s/%5C//g;
-
-    require URI::Escape;
-    $lnk = URI::Escape::uri_unescape ($lnk);
-
-    # Set video properties.
-
-    $$props->video_id   ($tmp->{id});
-    $$props->page_title (undef, $tmp->{title});
-    $$props->video_link ($lnk);
+    $$props->video_id( $r->{id} );
+    $$props->page_title( undef, $r->{title} );
+    $$props->video_link( $r->{url} );
 
     return 0;
 }
 
-sub parseVideoURL {
+sub getURL {
+    my ( $self, $r, $content ) = @_;
 
-    my ($self, $content) = @_;
-    my $lnk;
-
-    # Match available formats.
-
-    my $re = qr|%22(\w\w)URL%22%3A%22(.*?)%22|;
-    my %lst = $$content =~ /$re/gm;
-
-    if (keys %lst == 0) {
-        clive::Log->instance->err(CLIVE_REGEXP, "no match: `$re'");
-        return (1, $lnk);
+    if ( $$content !~ /"sequence",\s+"(.*?)"/ ) {
+        my $e = "no match: sequence";
+        if ( $$content =~ /"_partnerplayer"/ ) {
+            $e .=
+              ": looks like a partner video which we do not support";
+        }
+        clive::Log->instance->err( CLIVE_REGEXP, $e );
+        return 1;
     }
 
-    # User requested format.
+    require URI::Escape;
+    my $seq = URI::Escape::uri_unescape($1);
 
-    my $format = clive::Config->instance->config->{format};
-    $format    = "sd" if $format eq "default"; # sd=default
-
-    # Match requested format to a video link.
-
-    foreach (qw/sd hq hd/) {
-
-        if (not exists $lst{$_}) {
-            print STDERR "warning: `$_' not found in hash, ignored.\n";
-            next;
+    if ( $seq !~ /"videoPluginParameters":\{(.*?)\}/ ) {
+        if ( $$content !~ /"video", "(.*?)"/ ) {
+            clive::Util::Log->err( CLIVE_REGEXP,
+                "no match: video plugin params" );
+            return 1;
         }
+        else {
 
-        if ($format eq "best")
-            { $lnk = $lst{$_}; }
+            # Some videos (that require setting family_filter cookie)
+            # may list only one link which is not found under
+            # "videoPluginParameters". See also:
+            # http://sourceforge.net/apps/trac/clive/ticket/4
+            $$r->{url} = $1;
+            return 0;
+        }
+    }
 
-        elsif ($format eq $_) {
-            $lnk = $lst{$_};
+    my $vpp = $1;
+
+    # "sd" is our "default".
+    my $req_fmt = clive::Config->instance->config->{format};
+    $req_fmt = "sd" if $req_fmt eq "default";
+
+    # Choose "best" from the array. Check against the reported video
+    # resolution (height). Pick the highest available.
+    my ( $best, $curr );
+    my $best_h = 0;
+
+    my $re = qr|(\w+)URL":"(.*?)"|;
+    my %h = $vpp =~ /$re/gm;
+
+    while ( my ( $id, $url ) = each(%h) ) {
+        $url =~ tr{\\}{}d;
+
+        # Found the requested format ID.
+        if ( $id eq $req_fmt ) {
+            $curr = $url;
             last;
         }
 
+        # Default to whatever is the first in the array.
+        $curr ||= $url;
+
+        # Compare height with current (best) height.
+        if ( $url =~ /(\d+)x(\d+)/ ) {
+            if ( $2 > $best_h ) {
+                $best_h = $2;
+                $best   = $url;
+            }
+        }
     }
 
-    # parsePage will take care of checking $lnk value.
+    # If the user requested "best", set whatever we found as such above.
+    $curr = $best if $req_fmt eq "best";
 
-    return (0, $lnk);
+    $$r->{url} = $curr;
+
+    return 0;
 }
 
 1;
